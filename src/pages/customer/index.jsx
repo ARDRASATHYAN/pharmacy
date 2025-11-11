e7cd6fe1e9d9911456b3be86dc6f6bb9e22bde8f
ARDRASATHYAN <sardra9988@gmail.com>
2025-11-11 11:29:32 +0530

    fix:button name changed form the customer index.jsx

--- src/pages/customer/index.jsx
+++ src/pages/customer/index.jsx
@@ -6,99 +6,99 @@ import CustomerForm from "./components/CustomerForm";
 import { getCustomerColumns } from "./components/CustomerHeader";
 
 export default function CustomerMockApiHeader() {
-  const { data: customer = [], isLoading } = useCustomer();
-  const addCustomer = useAddCustomer();
-  const updateCustomer = useUpdateCustomer();
-  const deleteCustomer = useDeleteCustomer();
+    const { data: customer = [], isLoading } = useCustomer();
+    const addCustomer = useAddCustomer();
+    const updateCustomer = useUpdateCustomer();
+    const deleteCustomer = useDeleteCustomer();
 
-  const [open, setOpen] = useState(false);
-  const [editMode, setEditMode] = useState(false);
-  const [formData, setFormData] = useState({
-     customer_name:"", address:"",gst_no:"",phone:"" ,email:"",doctor_name:"",prescription_no:""
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
-
-  // 🟢 Add or Update
-  const handleSubmit = () => {
-    if (editMode) {
-      updateCustomer.mutate(
-        { id: formData.customer_id, data: formData },
-        {
-
-          onSuccess: () => setOpen(false),
-        }
-      );
-    } else {
-      addCustomer.mutate(formData, {
-        onSuccess: () => setOpen(false),
-      });
-    }
-  };
-
-
-
-  // ✏️ Edit Handler
-
-  const handleEdit = (row) => {
-
-    console.log("row", row);
-
-    setFormData(row);
-    setEditMode(true);
-    setOpen(true);
-  };
-
-  // ❌ Delete Handler
-  const handleDelete = (id) => {
-    if (window.confirm("Are you sure you want to delete this user?")) {
-      deleteCustomer.mutate(id);
-    }
-  };
-
-
-  if (isLoading) return <p>Loading hsns...</p>;
-
-
-  // ✅ pass handlers to columns (so edit/delete buttons work)
-  const columns =getCustomerColumns(handleEdit, handleDelete);
-
-  return (
-    <>
-      <div style={{ display: "flex", justifyContent: "space-between", marginBottom: 10 }}>
-        <h2 className="text-xl font-bold text-blue-700 tracking-wide">
-          Customer List
-        </h2>
-        <Button variant="contained" color="primary" onClick={() => {
-    // 🧹 Clear previous data before opening
-    setFormData({
-     customer_name:"", address:"",gst_no:"",phone:"" ,email:"",doctor_name:"",prescription_no:""
+    const [open, setOpen] = useState(false);
+    const [editMode, setEditMode] = useState(false);
+    const [formData, setFormData] = useState({
+        customer_name: "", address: "", gst_no: "", phone: "", email: "", doctor_name: "", prescription_no: ""
     });
-    setEditMode(false);
-    setOpen(true);
-  }}>
-          Add supplier
-        </Button>
-      </div>
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+    };
+
+    // 🟢 Add or Update
+    const handleSubmit = () => {
+        if (editMode) {
+            updateCustomer.mutate(
+                { id: formData.customer_id, data: formData },
+                {
+
+                    onSuccess: () => setOpen(false),
+                }
+            );
+        } else {
+            addCustomer.mutate(formData, {
+                onSuccess: () => setOpen(false),
+            });
+        }
+    };
 
 
-      <BasicTable columns={columns} data={customer} />
+
+    // ✏️ Edit Handler
+
+    const handleEdit = (row) => {
+
+        console.log("row", row);
+
+        setFormData(row);
+        setEditMode(true);
+        setOpen(true);
+    };
+
+    // ❌ Delete Handler
+    const handleDelete = (id) => {
+        if (window.confirm("Are you sure you want to delete this user?")) {
+            deleteCustomer.mutate(id);
+        }
+    };
 
 
-      <CustomerForm
-        open={open}
-        onClose={() => {
-          setOpen(false);
-          setEditMode(false);
-        }}
-        onSubmit={handleSubmit}
-        formData={formData}
-        onChange={handleChange}
-        editMode={editMode}
-      />
-    </>
-  );
+    if (isLoading) return <p>Loading hsns...</p>;
+
+
+    // ✅ pass handlers to columns (so edit/delete buttons work)
+    const columns = getCustomerColumns(handleEdit, handleDelete);
+
+    return (
+        <>
+            <div style={{ display: "flex", justifyContent: "space-between", marginBottom: 10 }}>
+                <h2 className="text-xl font-bold text-blue-700 tracking-wide">
+                    Customer List
+                </h2>
+                <Button variant="contained" color="primary" onClick={() => {
+                    // 🧹 Clear previous data before opening
+                    setFormData({
+                        customer_name: "", address: "", gst_no: "", phone: "", email: "", doctor_name: "", prescription_no: ""
+                    });
+                    setEditMode(false);
+                    setOpen(true);
+                }}>
+                    Add Customer
+                </Button>
+            </div>
+
+
+            <BasicTable columns={columns} data={customer} />
+
+
+            <CustomerForm
+                open={open}
+                onClose={() => {
+                    setOpen(false);
+                    setEditMode(false);
+                }}
+                onSubmit={handleSubmit}
+                formData={formData}
+                onChange={handleChange}
+                editMode={editMode}
+            />
+        </>
+    );
 }
